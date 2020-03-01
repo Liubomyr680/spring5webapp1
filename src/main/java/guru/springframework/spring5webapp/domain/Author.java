05be17ace71000405df288f38f78a0b4a28f458b
Liubomyr680 <lshynkaruk8@gmail.com>
2020-03-01 09:44:23 +0200

    Create two classes, and do a ManyToMany relationship with Author and Book classes. Implement the equals and hashcode methods, and toString method

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -2,6 +2,7 @@ package guru.springframework.spring5webapp.domain;
 
 
 import javax.persistence.*;
+import java.util.Objects;
 import java.util.Set;
 @Entity
 public class Author {
@@ -55,5 +56,28 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(id, author.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
